62e6ae24d9b894ebec2ca6488e7d31ea42731b7f
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 09:19:17 +0530

    Average no first commit

---
+++ averageognumber.py
@@ -0,0 +1,6 @@
+#WAP to input 2 floating point numbers & print their average
+
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
+average = (num1 + num2) / 2
+print("Average Of Two Numbers Is: ",average)
