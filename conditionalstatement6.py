03a126e02df45451aadf52dfcd68c84697d041dd
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 14:14:11 +0530

    odd or even number

---
+++ conditionalstatement6.py
@@ -0,0 +1,8 @@
+#WAP to check if a number entered by user is odd or even
+
+number =int(input("Enter a number to check: "))
+
+if(number % 2 ==0 ):
+    print("No.is even")
+else:
+    print("No.is odd")
