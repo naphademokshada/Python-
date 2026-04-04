7590b6284101b90b52fd48da895b12a75851d87e
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 11:26:35 +0530

    Capitalize function of python

---
+++ function2.py
@@ -0,0 +1,5 @@
+# capitalize function
+str1 = "i am Studying python from Apna College"
+
+print(str1.capitalize()) # Capitalizes the first character of the string and converts the rest to lowercase
+print(str1) # Original string remains unchanged
