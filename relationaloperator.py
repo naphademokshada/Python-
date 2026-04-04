ffe1ea79ef90c7acec78900d0ffdd77e9e558d31
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 09:02:14 +0530

    Relational Operator

---
+++ relationaloperator.py
@@ -0,0 +1,11 @@
+#relational operator
+
+a=52
+b = 20
+
+print(a ==b) #equal to
+print(a !=b) #not equal to  
+print(a >b) #greater than
+print(a <b) #less than  
+print(a >=b) #greater than or equal to
+print(a <=b) #less than or equal to
