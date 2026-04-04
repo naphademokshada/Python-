d9219b8581173f6193895e1e687479396a7d4068
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 09:05:26 +0530

    Input

---
+++ input.py
@@ -0,0 +1,11 @@
+name =input("enter your name:")
+print("my name is: ",name)
+
+val= input("enter a number: ")
+print(type(val),val)
+
+int("5")
+val= int(input("enter a number: "))
+print(type(val),val)
+
+
