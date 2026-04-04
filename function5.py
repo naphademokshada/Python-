7e45ccb63969ffe623d68d272774505af1ec878f
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 12:36:47 +0530

    Count function

---
+++ function5.py
@@ -0,0 +1,5 @@
+#count function
+str1 = "i am Studying python from Apna College"
+print(str1.count("a")) # Counts the number of occurrences of "a" in the string
+print(str1.count("python")) # Counts the number of occurrences of "python" in the string
+print(str1.count("java")) # Counts the number of occurrences of "java" in the
