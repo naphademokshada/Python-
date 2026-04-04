0dfe31938dbf0e56a2f3a3ea543e09403b16fdd0
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 12:34:56 +0530

    Find function

---
+++ function4.py
@@ -0,0 +1,5 @@
+#find function
+
+str1 = "i am Studying python from Apna College"
+print(str1.find("python")) # Returns the index of the first occurrence of "python" in the string, or -1 if not found
+print(str1.find("java")) # Returns -1 since "java" is not found in the string
