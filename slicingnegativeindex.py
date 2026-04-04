e5dd434307851eb677eae6a209764887805bb56e
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 11:23:41 +0530

    Slicing of negative index

---
+++ slicingnegativeindex.py
@@ -0,0 +1,6 @@
+#Negative Index Slicing
+
+str = "Mokshada Naphade"
+print(str[-3:-1]) # from end to 2nd last character
+print(str[-5:]) # from 5th last character to end
+print(str[: -5]) # from start to 5th last character
