2badb47fe2ad4ebbe43e660258f8cd34008807bb
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 09:15:19 +0530

    Sum of number

---
+++ Sumofnumber.py
@@ -0,0 +1,6 @@
+#Write a program to input 2 numbers & print their sum
+
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+sum = num1 + num2
+print("Sum of ",num1," and ",num2," is: ",sum)
