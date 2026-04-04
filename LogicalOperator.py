1a44f9796f0ca8c4879d320f1f3e86767be824df
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 09:00:30 +0530

    Logical Operator

---
+++ LogicalOperator.py
@@ -0,0 +1,13 @@
+#LogicalOperator
+ 
+a = 50
+b = 30
+print(not True) #not operator
+print(not (a>b)) #not operator
+
+
+val1 = False
+val2 = False
+print("AND Operator:", val1 and val2) #and operator
+
+print("OR Operator:", a == b or a > b)
