1015abc65c0e9ecc38a932c0a04851011f3a0d5d
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 08:59:38 +0530

    Assignement Opeartor

---
+++ AssignmentOperator.py
@@ -0,0 +1,16 @@
+#**
+
+#assignemnt operator
+
+num = 10
+num += 10
+"""sub -= 10
+modulo %= 10
+div /= 10
+num *= 10
+print("num: ",sum)
+print("num: ",sub)
+print("num: ",modulo)
+print("num: ",num)  """
+
+print("num: ",num)
