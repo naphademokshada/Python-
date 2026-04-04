ae35058755316d5f26eac83dd5877fc6d8b22305
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 09:09:04 +0530

    input 1

---
+++ input1.py
@@ -0,0 +1,7 @@
+name = input("Enter Name:")
+age = int(input("Enter Age:")  ) 
+marks = float(input("Enter Marks:"))
+
+print("My Name Is: ",name)
+print("My Age Is: ",age)
+print("My Marks Are: ",marks)
