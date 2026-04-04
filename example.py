c47f6f4179f19e479b187d0369471dffaf8edbca
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 09:19:45 +0530

    example of python

---
+++ example.py
@@ -0,0 +1,7 @@
+#WAP to input 2 int numbers , a and b 
+#Print True if Greater than or equal to b . If not print false
+
+a = int(input("Enter First Number:"))
+b = int(input("Enter Second Number"))
+
+print(a >= b) # True if a is greater than or equal to b, otherwise False
