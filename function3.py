3b21bada8cb7e5e51d5f9b100cdfbb3d6eb1e23b
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 11:28:46 +0530

    Replace function

---
+++ function3.py
@@ -0,0 +1,6 @@
+# replace Function
+
+str1 = "i am Studying python from Apna College"
+print(str1.replace("o", "a")) # Replaces all occurrences of "o" with "a"
+print(str1.replace("python", "java")) # Replaces "python" with "java"
+print(str1) # Original string remains unchanged
