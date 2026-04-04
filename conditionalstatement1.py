b8f1efd7da35ec079cdffaab5e629b496b9d6b54
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 14:08:51 +0530

    eligible for voting and driving or not

--- conditionalstatement1.py
+++ conditionalstatement1.py
@@ -2,3 +2,5 @@ age = 21
 
 if (age >= 18):  #if(True) also allowed
     print("You are eligible to vote and Apply for a driving license.")
+else:
+    print("You are not eligible to vote and Apply for a driving license.")
