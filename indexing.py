ad3605d2fab9f7fd18643e493d6920a3271f396a
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 11:22:33 +0530

    Indexing of Sentence or word first commit

---
+++ indexing.py
@@ -0,0 +1,7 @@
+#Indexing
+str = "Welcome to Mokshada Page"
+
+# str[4]="@" #string is immutable we cannot change the value of string , only access character not allowed to manipulated char
+print(str[0])
+print(str[11])
+print(str[20])
