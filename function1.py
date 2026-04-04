59181263e85ad621f88d79b74026ad636c885bb9
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 11:25:20 +0530

    endswith function python

---
+++ function1.py
@@ -0,0 +1,4 @@
+#enswith function
+str1 = "I am Studying python from Apna College"
+print(str1.endswith("ege")) # True if string ends with "Apna College", otherwise False
+print(str1.endswith("te"))
