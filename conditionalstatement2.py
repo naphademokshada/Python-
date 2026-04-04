849a5e817bb73f2639d885654a28a385d9f29635
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 14:09:26 +0530

    Signal Light

---
+++ conditionalstatement2.py
@@ -0,0 +1,11 @@
+light = "green"
+
+if(light == "red"):
+    print("Stop")
+elif(light == "yellow"):
+    print("Ready")
+elif(light == "green"):
+    print("Go")
+else:   
+    print("Invalid light color")
+print("End of program")
