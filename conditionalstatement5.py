88d6ca6dab120fdb5f5c0c5a80fcace70414e8af
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-04 14:11:38 +0530

    grade of students based on marks

---
+++ conditionalstatement5.py
@@ -0,0 +1,10 @@
+age = 95
+
+#nesting
+if(age>=18):
+    if(age>=81):
+        print("You are a senior citizen")
+    else:
+        print("You can drive")
+else:
+    print("You are not eligible to drive")
