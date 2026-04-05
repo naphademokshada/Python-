963e7f2f95b8a25f3b1e9c62a156627f3903cf32
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-05 12:37:09 +0530

    index and count method in tuple

---
+++ tuple4.py
@@ -0,0 +1,6 @@
+#Tuple methods
+tup = (1,3,4,2,5,2,2)
+#tup.index(3) # it will give index of 3
+#tup.count(1) # it will give count of 1
+print(tup.index(3)) #it is at index 1
+print(tup.count(2)) # it is 3 times in the tuple
