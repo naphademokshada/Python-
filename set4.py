52f25bc21b0d78f1e1c5b16495d29a6df001077f
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-05 16:09:50 +0530

    Length function in set (method

---
+++ set4.py
@@ -0,0 +1,8 @@
+# method in set
+collection = set()
+collection.add(1)
+collection.add(2)
+collection.add(2)
+collection.add("hello")
+
+print(len(collection)) # 3 because duplicate value is ignored
