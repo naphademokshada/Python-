ea4d7f3329dbca971513a91c8484055e713cd409
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-05 12:40:51 +0530

    Dictionary print

---
+++ dict1.py
@@ -0,0 +1,10 @@
+info = {
+    "name" : "Mokshada Naphade",
+    "email" : "mokshadanaphade@gmail.com",
+    "mobile" : "9028080808",
+    "city" : "Pune",
+    "topics" : ["Python","Java","SQL"],
+    "age" : 20,
+}
+
+print(info)
