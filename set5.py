7177231f3be6669068650fedd5d91f7f3779104b
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-05 16:10:28 +0530

    Method in set(collection.pop

---
+++ set5.py
@@ -0,0 +1,6 @@
+#method
+
+collection = {"hello","Mokshada","Coding", "python"}
+
+print(collection.pop()) #removes and returns a random element from the set
+print(collection).pop() #removes and returns a random element from the set
