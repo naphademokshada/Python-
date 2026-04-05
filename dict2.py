cc3f8b556ca6008a34b30738a62a8754deb6b716
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-05 12:41:31 +0530

    Length function in Dictionary

---
+++ dict2.py
@@ -0,0 +1,9 @@
+student = {
+    "name" : "Mokshada Naphade",
+    "subjects" : {
+            "Python" : 80,
+            "Java" : 90,
+            "SQL" : 85
+     }
+}
+print(len(list(student))) # it will give number of keys in the dictionary
