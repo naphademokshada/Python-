dd9ba550598224732ecaa24f5f3df7e0f7502754
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-05 16:12:56 +0530

    whileloop hello word

---
+++ loop1.py
@@ -0,0 +1,15 @@
+#Loops
+#while loop to print hello word
+count = 1
+while count <= 5:
+    print("Hello")
+    count +=1
+
+print(count)
+    
+   
+count = 100
+while count >= 100:
+    print("Hello")
+    count += 1
+print(count)
