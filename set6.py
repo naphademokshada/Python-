4aefdd18b40fbe44eec1a9a7ecb949ce602107fb
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-05 16:11:19 +0530

    methods in Set 1

---
+++ set6.py
@@ -0,0 +1,10 @@
+#method
+
+set1 = {1,2,3}
+set2 = {2,3,4}
+
+print(set1.union(set2)) # {1,2,3,4}
+print(set1.intersection(set2)) # {2,3}
+print(set1.difference(set2)) # {1}
+print(set1.symmetric_difference(set2)) # {1,4}
+
