e6b979cf9959a260852b37ce2b1fe0aeb5fd326d
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-05 12:46:19 +0530

    Store meaning in dictionary using list

---
+++ DictProgram1.py
@@ -0,0 +1,6 @@
+#Store following word meanings in a python dictionary 
+dictionary = {
+     "table" : ["a piece of furniture","list of facts and figures"],
+     "cat" :  "a small animal"
+}
+print(dictionary)
