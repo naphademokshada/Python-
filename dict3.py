36ee27a178b14a363332512d21ecdf9590646bb8
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-05 12:42:08 +0530

    Keys , values , items function in Dict

---
+++ dict3.py
@@ -0,0 +1,12 @@
+student = {
+    "name" : "Mokshada Naphade",
+    "subject" : {
+        "physics" : 80,
+        "chemistry" : 90,
+        "maths" : 95
+    }
+}
+
+print(list(student.keys())) # it will give all the keys in the dictionary
+print(list(student.values())) # it will give all the values in the dictionary
+print(list(student.items())) # it will give all the key value pairs in the dictionary as a
