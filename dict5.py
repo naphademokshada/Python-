38744b015853fd0ec97f1bf65c2a7703e0c70db8
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-05 12:43:24 +0530

    list in dictionary how to used and update name

---
+++ dict5.py
@@ -0,0 +1,10 @@
+info = {
+    "name" : "Mokshada Naphade",
+    "subject" : ["physics","chemistry","maths"],
+    "topics" : ["dictionary","list","tuple"],
+    "age" : 20,
+    }
+
+info["name"] = "Padmavati"
+
+print(info)
