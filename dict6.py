07770c5ecda54ce79080dbbc71d8482c01d69190
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-05 12:44:11 +0530

    Dictionary item method

---
+++ dict6.py
@@ -0,0 +1,13 @@
+#item methods
+student = {
+    "name" : "Mokshada Naphade",
+
+    "subject" : {
+        "physics" : 80,
+        "chemistry" : 90,
+        "maths" : 95
+    }
+}
+
+pairs = list(student.items())
+print(pairs[1])
