672c5de1d29c2d0990f5c4b6de5073138c339f25
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-05 12:25:02 +0530

    How to print list

---
+++ list1.py
@@ -0,0 +1,7 @@
+# List and Tuples
+
+marks = [12, 45, 67, 89, 90 , 95.2 , 66.4] # List of marks
+print(marks) # Print the entire list
+print(type(marks)) # Print the type of the variable 'marks'
+print(marks[0]) # Accessing the first element of the list
+print(marks[2]) # Accessing the third element of the list   
