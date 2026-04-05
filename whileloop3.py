69d3398ffe9e456cac4a37e5bd527d7d4e525cb8
Mokshada Naphade <mokshadanaphade@gmail.com>
2026-04-05 16:16:50 +0530

    print elements in list

---
+++ whileloop3.py
@@ -0,0 +1,13 @@
+#Print the element of the following list using loop :
+[1,4,9,16,25,36,49,64,81,100]
+
+nums = [1,4,9,16,25,36,49,64,81,100]
+
+#traverse
+idx = 0
+while idx < len(nums):
+    print(nums[idx]) # nums[0], nums[1], nums[2]....nums[9]
+    idx += 1
+    
+    
+
